a929e14f062458a2742856544256331d30f25bf2
Marian Andonov <marianandonov307@gmail.com>
2020-08-13 14:37:06 +0300

    ...

---
+++ BFS/BFSTask.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BFSTask
+{
+
+    public class TreeNode<T>
+    {
+        private T value;
+        private bool hasParent;
+        public List<TreeNode<T>> children;
+
+        public TreeNode(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(
+                      "Cannot insert null value!");
+            }
+            this.value = value;
+            this.children = new List<TreeNode<T>>();
+        }
+
+        public T Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+        public bool IsVisited { get; set; }
+    }
+
+    public static int BFS(TreeNode<T> start, TreeNode<T> end)
+    {
+        Queue<TreeNode<T>> queue = new Queue<TreeNode>();
+        queue.Enqueue(start);
+
+        while (queue.Count != 0)
+        {
+            TreeNode CurrentNode = queue.Dequeue();
+            CurrentNode.IsVisited = true;
+
+            foreach (Node Child in CurrentNode.Children.Keys)
+            {
+                if (Child.IsVisited == false)
+                {
+                    Child.IsVisited = true;
+                    if (Child == end) return 1;
+                }
+                queue.Enqueue(Child);
+            }
+        }
+        return 0;
+    }
+}
--- Labyrinth/Labyrinth.cs
+++ Labyrinth/Labyrinth.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Labyrinth
 {
@@ -11,35 +15,65 @@ namespace Labyrinth
                 {'*', '*', ' ', '*', ' ', '*', ' '},
                 {' ', ' ', ' ', ' ', ' ', ' ', ' '},
                 {' ', '*', '*', '*', '*', '*', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', 'е'},
+                {' ', ' ', ' ', ' ', ' ', ' ', 'е'}
             };
-        static void Main()
-        {
-            FindPath(0, 0);
-        }
 
-        static void FindPath(int row, int col)
+
+
+        static char[] path = new char[lab.GetLength(0) * lab.GetLength(1)];
+        static int position = 0;
+        static int counter = 0;
+        static List<int> lengths = new List<int>();
+        static List<string> list = new List<string>();
+
+        static void FindPath(int row, int col, char direction)
         {
 
             if ((col < 0) || (row < 0) ||
-                  (col >= lab.GetLength(1)) || (row >= lab.GetLength(0));
+                  (col >= lab.GetLength(1)) || (row >= lab.GetLength(0)))
             {
                 return;
             }
+
+            path[position] = direction;
+            position++;
+
             if (lab[row, col] == 'е')
             {
-                Console.WriteLine("Found the exit!");
+                list.Add(GetMaxPath(path, 1, position - 1));
+                counter++;
             }
-            if (lab[row, col] != ' ')
+
+            if (lab[row, col] == ' ')
             {
-                return;
+                lab[row, col] = 's';
+                FindPath(row, col - 1, 'L'); // left
+                FindPath(row - 1, col, 'U'); // up
+                FindPath(row, col + 1, 'R'); // right
+                FindPath(row + 1, col, 'D'); // down
+                lab[row, col] = ' ';
             }
-            lab[row, col] = 's';
-            FindPath(row, col - 1);
-            FindPath(row - 1, col);
-            FindPath(row, col + 1);
-            FindPath(row + 1, col);
-            lab[row, col] = ' ';
+
+            position--;
+
+            lengths.Add(list[counter].Length);
+        }
+
+        static string GetMaxPath(char[] path, int startPos, int endPos)
+        {
+            string s = "";
+            for (int pos = startPos; pos <= endPos; pos++)
+            {
+                s += path[pos];
+            }
+            return s;
+        }
+
+        static void Main()
+        {
+            FindPath(0, 0, 'S');
+            int max = lengths.Max;
+            Console.WriteLine($"The longest path is {max} moves long!");
         }
     }
 }
